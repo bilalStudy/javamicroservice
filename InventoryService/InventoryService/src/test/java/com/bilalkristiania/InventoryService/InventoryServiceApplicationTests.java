e9791824f6cb864977fbc63d27afadd5119e22cc
BilalStudy <biah001@student.kristiania.no>
2023-10-02 09:40:31 +0200

    uodating some readme files, and addind a stock service. might be removed later

---
+++ InventoryService/InventoryService/src/test/java/com/bilalkristiania/InventoryService/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilalkristiania.InventoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
