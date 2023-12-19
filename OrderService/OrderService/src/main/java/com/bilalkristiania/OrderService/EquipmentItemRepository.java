8693d28de4ced7802e78d89b81dfeed27242a33f
bilalStudy <biah001@student.kristiania.no>
2023-12-19 04:46:40 +0100

    docker things

--- OrderService/OrderService/src/main/java/com/bilalkristiania/OrderService/EquipmentItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.bilalkristiania.OrderService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipmentItemRepository extends JpaRepository<EquipmentItem, Long> {
-}
--- OrderService/OrderService/src/main/java/com/bilalkristiania/OrderService/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bilalkristiania.OrderService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
